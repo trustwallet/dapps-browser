adc13372026d5f64d085ea41c444edf71126b4b8
Cyrus Goh <hello@lovincyrus.com>
2018-02-12 16:58:53 -0800

    Test logic on componentWillMount

--- src/DApps.js
+++ src/DApps.js
@@ -13,18 +13,18 @@ export const marketsList = [
         "category": "social"
     },
     {
-        "name": "Leeroy",
-        "url": "https://leeroy.io",
-        "description": "Leeroy is a decentralized social network build on Ethereum.",
-        "image": "https://ipfs.infura.io/ipfs/Qmb2YsYgyp6joBGJ9KrRcqGhDtbLjDdWCq3LFgv2duYReg",
-        "category": "social"
-    },
-    {
         "name": "OpenSea",
         "url": "https://opensea.io/",
         "description": "Peer-to-peer marketplace for rare digital items",
         "image": "https://opensea.io/static/images/opensea-icon.png",
         "category": "marketplaces"
+    },
+    {
+        "name": "Leeroy",
+        "url": "https://leeroy.io",
+        "description": "Leeroy is a decentralized social network build on Ethereum.",
+        "image": "https://ipfs.infura.io/ipfs/Qmb2YsYgyp6joBGJ9KrRcqGhDtbLjDdWCq3LFgv2duYReg",
+        "category": "social"
     },
     {
         "name": "Name Bazaar",
@@ -33,13 +33,13 @@ export const marketsList = [
         "image": "https://namebazaar.io/images/logo@2x.png",
         "category": "marketplaces"
     },
-    {
-        "name": "Cryptokitties",
-        "url": "https://www.cryptokitties.co/",
-        "description": "Collect and breed adorable digital cats",
-        "image": "https://www.cryptokitties.co/images/letterHead.png",
-        "category": "games"
-    }
+    // {
+    //     "name": "Cryptokitties",
+    //     "url": "https://www.cryptokitties.co/",
+    //     "description": "Collect and breed adorable digital cats",
+    //     "image": "https://www.cryptokitties.co/images/letterHead.png",
+    //     "category": "games"
+    // }
 ]
 
 
@@ -51,6 +51,20 @@ class DApps extends React.Component {
         // array will be empty
     }
 
+    // extra logic to update state
+    componentWillMount() {
+        const myData = [].concat(this.state.data)
+          .sort((a, b) => a.itemM > b.itemM)
+          .map((item, index) => 
+          <div key={index}>
+            {item.category}
+          </div>
+        );
+
+        this.setState({ myData });
+        console.log(myData);
+    }
+
     render() {
       console.log(this.state.data)
         return (
@@ -59,7 +73,7 @@ class DApps extends React.Component {
                 <div key={index}>
                   <h2 className="categories">{dapp.category}</h2>
                   <Media className="mt-1 align">
-                    <Media left bottom href="/">
+                    <Media left bottom href={dapp.url}>
                       <img src={dapp.image} alt="logo"/>
                     </Media>
                     <Media body>
