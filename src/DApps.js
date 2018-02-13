2d3b33a49d0f0490bea8a7cde35ef3d48e7652a2
Viktor Radchenko <yazexel@gmail.com>
2018-02-13 02:58:24 -0800

    Render items in the list

--- src/DApps.js
+++ src/DApps.js
@@ -6,40 +6,47 @@ import './App.css';
 
 export const marketsList = [
     {
-        "name": "Cent",
-        "url": "https://beta.cent.co/",
-        "description": "Give wisdom, get money. Ask a question and offer and bounty for the best answers.",
-        "image": "https://beta.cent.co/img/logo_crop.png",
-        "category": "social"
+        "name": "Token Factory",
+        "url": "https://tokenfactory.netlify.com/",
+        "description": "Issue & Interact with Standard Token Contracts on Ethereum.",
+        "image": "https://tokenfactory.netlify.com/images/icon.png",
+        "category": "utility"
     },
-    {
-        "name": "OpenSea",
-        "url": "https://opensea.io/",
-        "description": "Peer-to-peer marketplace for rare digital items",
-        "image": "https://opensea.io/static/images/opensea-icon.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "Leeroy",
-        "url": "https://leeroy.io",
-        "description": "Leeroy is a decentralized social network build on Ethereum.",
-        "image": "https://ipfs.infura.io/ipfs/Qmb2YsYgyp6joBGJ9KrRcqGhDtbLjDdWCq3LFgv2duYReg",
-        "category": "social"
-    },
-    {
-        "name": "Name Bazaar",
-        "url": "https://namebazaar.io",
-        "description": "ENS name marketplace",
-        "image": "https://namebazaar.io/images/logo@2x.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "Cryptokitties",
-        "url": "https://www.cryptokitties.co/",
-        "description": "Collect and breed adorable digital cats",
-        "image": "https://www.cryptokitties.co/images/letterHead.png",
-        "category": "games"
-    }
+    // {
+    //     "name": "Cent",
+    //     "url": "https://beta.cent.co/",
+    //     "description": "Give wisdom, get money. Ask a question and offer and bounty for the best answers.",
+    //     "image": "https://beta.cent.co/img/logo_crop.png",
+    //     "category": "social"
+    // },
+    // {
+    //     "name": "OpenSea",
+    //     "url": "https://opensea.io/",
+    //     "description": "Peer-to-peer marketplace for rare digital items",
+    //     "image": "https://opensea.io/static/images/opensea-icon.png",
+    //     "category": "marketplaces"
+    // },
+    // {
+    //     "name": "Leeroy",
+    //     "url": "https://leeroy.io",
+    //     "description": "Leeroy is a decentralized social network build on Ethereum.",
+    //     "image": "https://ipfs.infura.io/ipfs/Qmb2YsYgyp6joBGJ9KrRcqGhDtbLjDdWCq3LFgv2duYReg",
+    //     "category": "social"
+    // },
+    // {
+    //     "name": "Name Bazaar",
+    //     "url": "https://namebazaar.io",
+    //     "description": "ENS name marketplace",
+    //     "image": "https://namebazaar.io/images/logo@2x.png",
+    //     "category": "marketplaces"
+    // },
+    // {
+    //     "name": "Cryptokitties",
+    //     "url": "https://www.cryptokitties.co/",
+    //     "description": "Collect and breed adorable digital cats",
+    //     "image": "https://www.cryptokitties.co/images/letterHead.png",
+    //     "category": "games"
+    // }
 ]
 
 
@@ -52,35 +59,52 @@ class DApps extends React.Component {
     componentWillMount() {
         const myData = [].concat(this.state.data)
           .sort((a, b) => a.category > b.category)
-        // console.log(myData);
 
-        this.setState({ myData });
+        let categories = {}
+        myData.forEach((item) => {
+          let list = categories[item.category] || []
+          list.push(item)
+          categories[item.category] = list
+        })
+
+        this.setState({ myData: categories });
     }
 
     render() {
-      // console.log(this.state.data)
+      const keys = Object.keys(this.state.myData)
         return (
             <div className="DApps">
-              {this.state.myData.map((dapp, index) => (
-                <div key={index}>
-                  <h2 className="categories">{dapp.category}</h2>
-                  <Media className="mt-1 align">
-                    <Media left bottom href={dapp.url}>
-                      <img src={dapp.image} alt="logo"/>
-                    </Media>
-                    <Media body>
-                    <Media heading>
-                      <a href={dapp.url}>{dapp.name}</a>
-                    </Media>
-                    {dapp.description}
-                    </Media>
-                  </Media>
+              {keys.map((category, index) => (
+               <div key={index}>
+               <h2 className="categories">{category}</h2>
+                <Items key ={index} items = {this.state.myData[category]}  />
                 </div>
               ))}
             </div>
         )
     }
-    
+}
+
+class Items extends React.Component {
+  render() {
+      return (
+          <div>
+            {this.props.items.map((dapp, index) => (
+                <Media key={index} className="mt-1 align">
+                  <Media left bottom href={dapp.url}>
+                    <img src={dapp.image} alt="logo"/>
+                  </Media>
+                  <Media body>
+                  <Media heading>
+                    <a href={dapp.url}>{dapp.name}</a>
+                  </Media>
+                  {dapp.description}
+                  </Media>
+                </Media>
+              ))}
+          </div>
+      );
+  }
 }
 
 export default DApps;
