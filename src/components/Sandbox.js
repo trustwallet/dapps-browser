75c57ce818b395fa53c6bc00b335f0bac8996168
mishfit <mishochu@gmail.com>
2018-02-23 00:42:59 -0700

    better styling

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -9,11 +9,24 @@ class Sandbox extends React.Component {
     }
 
     render() {
+        let connectionStatusClass
+        switch (this.state.isConnected) {
+          case 'true':
+            connectionStatusClass = 'alert alert-success'
+            break;
+          case 'false':
+            connectionStatusClass = 'alert alert-danger'
+            break;
+          default:
+            connectionStatusClass = 'alert alert-secondary'
+        }
         return (
             <div className="sandbox">
               <h1 className="title">Sandbox</h1>
               <h4>Is Connected</h4>
-              <button onClick={this.checkIsConnected.bind(this)}>Check Status</button>&nbsp;<label>{this.state.isConnected}</label>
+              <button onClick={this.checkIsConnected.bind(this)}>Check Status</button>&nbsp;&nbsp;
+              <span className={connectionStatusClass} role="alert">{this.state.isConnected}</span>
+              <hr />
               <h4>Get Network ID</h4>
             </div>
         )
