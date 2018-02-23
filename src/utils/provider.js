8b689f93429ddf20fae4f519ac3114fe597e9e69
mishfit <mishochu@gmail.com>
2018-02-22 23:13:19 -0700

    use context instead of global

--- src/utils/provider.js
+++ src/utils/provider.js
@@ -1,16 +1,16 @@
 import Web3 from 'web3'
 
-const global = (typeof window === 'object') ? window : global
+const context = (typeof window === 'object') ? window : global
 
 let provider,
     bundledWeb3
 
 export function getProvider () {
   if (!provider) {
-    if (global.web3 === undefined) {
+    if (context.web3 === undefined) {
       throw Error('this application needs to run in a DApp browser')
     } else {
-      provider = global.web3.currentProvider
+      provider = context.web3.currentProvider
     }
   }
 
