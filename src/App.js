c2df7443b8ccc4eef60584cd526dd1e1f8589609
Cyrus Goh <hello@lovincyrus.com>
2018-02-28 17:07:50 -0800

    Add DApps Platform description

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import {
   Route,
   Link
 } from 'react-router-dom'
-import {
-  Col
-} from 'reactstrap';
+import { Col } from 'reactstrap';
 import logo from './logo_solid_square_blue.svg';
 import './App.css';
 
@@ -66,9 +64,12 @@ const Home = () => (
 
 const Browser = () => (
   <div>
-    <h3 className="title"><a href="/">DApps Browser</a></h3>
-      <DApps>
-      </DApps>
+    <h3 className="title"><a href="/">DApps Platform</a></h3>
+    <p className="DApp-desc">
+      This is a list of Decentralized applications.
+    </p>
+    <DApps>
+    </DApps>
   </div>
 );
 
