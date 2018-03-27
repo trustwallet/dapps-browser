e9aeba386f112e636531253f52fd08bbeeb5909e
Viktor Radchenko <yazexel@gmail.com>
2018-03-26 21:56:22 -0700

    Update

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -94,7 +94,7 @@ export const marketsList = [
     {
         "name": "Crypto Racing League",
         "url": "https://cryptoracingleague.io/",
-        "description": "Race Cars. Collectable cars on the blockchain.",
+        "description": "Build, Race and Trade collectible cars on the  blockchain.",
         "image": "/assets/cryptoracing.png",
         "category": "games"
     },
