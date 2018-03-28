b85913930ee887687b67e2c159de85f283ea3d2f
Viktor Radchenko <yazexel@gmail.com>
2018-03-28 03:44:19 -0700

    Update dapps view

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -5,132 +5,67 @@ import {
     Col
 } from 'reactstrap';
 import DAppItem from "./DAppItem"
+import { TrustClient } from "../network/TrustClient"
 
-export const marketsList = [
-    {
-        "name": "Etheremon",
-        "url": "https://www.etheremon.com/",
-        "description": "Digital world of monsters.",
-        "image": "/assets/etheremon.png",
-        "category": "games"
-    },
-    {
-        "name": "CryptoFighters",
-        "url": "https://cryptofighters.io/",
-        "description": "Digital fighters on the blockchain.",
-        "image": "/assets/cryptoFighters.png",
-        "category": "games"
-    },
-    {
-        "name": "Etherbay",
-        "url": "https://www.etherbay.com/",
-        "description": "Buy & Sell Crypto Assets",
-        "image": "/assets/efolio.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "Token Factory",
-        "url": "https://tokenfactory.netlify.com/",
-        "description": "Issue & Interact with Standard Token Contracts on Ethereum.",
-        "image": "https://tokenfactory.netlify.com/images/icon.png",
-        "category": "utility"
-    },
-    {
-        "name": "Cent",
-        "url": "https://beta.cent.co/",
-        "description": "Give wisdom, get money. Ask a question and offer and bounty for the best answers.",
-        "image": "https://beta.cent.co/img/logo_crop.png",
-        "category": "social"
-    },
-    {
-        "name": "OpenSea",
-        "url": "https://opensea.io/",
-        "description": "Peer-to-peer marketplace for rare digital items",
-        "image": "https://opensea.io/static/images/opensea-icon.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "RareBits",
-        "url": "https://rarebits.io/",
-        "description": "Buy, sell, and discover unique crypto assets",
-        "image": "/assets/rarebits.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "Name Bazaar",
-        "url": "https://namebazaar.io",
-        "description": "ENS name marketplace",
-        "image": "https://namebazaar.io/images/logo@2x.png",
-        "category": "marketplaces"
-    },
-    {
-        "name": "Cryptokitties",
-        "url": "https://www.cryptokitties.co/",
-        "description": "Collect and breed adorable digital cats",
-        "image": "https://www.cryptokitties.co/images/letterHead.png",
-        "category": "games"
-    },
-    {
-        "name": "Kyber Network",
-        "url": "https://web3.kyber.network",
-        "description": "Decentralized exchange for ERC20 tokens",
-        "image": "/assets/kyber.png",
-        "category": "exchanges"
-    },
-    {
-        "name": "The Bancor Network",
-        "url": "https://www.bancor.network",
-        "description": "Decentralized exchange for ERC20 tokens",
-        "image": "/assets/bancor.png",
-        "category": "exchanges"
-    },
-    {
-        "name": "ERC dEX",
-        "url": "https://ercdex.com/",
-        "description": "Decentralized exchange powered by 0x protocol",
-        "image": "/assets/ercdex.png",
-        "category": "exchanges"
-    },
-    
-    // {
-    //     "name": "EtherBots",
-    //     "url": "https://etherbots.io",
-    //     "description": "Decentralised Robot Wars Game",
-    //     "image": "/assets/etherbots.png",
-    //     "category": "games"
-    // }
-]
 
 class DApps extends React.Component {
+    
     constructor(props) {
       super(props);
-      this.state = { data: marketsList};
+      this.state = { data: []};
+      this.trustClient = new TrustClient()
     }
 
-    componentWillMount() {
-        const myData = [].concat(this.state.data)
-          .sort((a, b) => a.category > b.category)
+    fetchList() {
+        this.trustClient.fetchBootstrap().then( response => {
+            this.setState({ data: response.data});
+            this.sort()
+        });
+    }   
 
+    sort() {
+        const myData = [].concat(this.state.data.popular)
         let categories = {}
         myData.forEach((item) => {
-          let list = categories[item.category] || []
-          list.push(item)
-          categories[item.category] = list
+            let id = item.category[0].name
+            let list = categories[id] || []
+            list.push(item)
+            categories[id] = list
         })
+        this.setState({ 
+            myData: categories,
+            today: [].concat(this.state.data.today)
+        });
+    }
 
-        this.setState({ myData: categories });
+
+
+    componentWillMount() {
+        
+
+        this.fetchList()
     }
 
     render() {
-      const keys = Object.keys(this.state.myData)
+    console.log(this.state.myData);
+      const keys = Object.keys(this.state.myData || {})
+      const today = (this.state.today || []).slice(0, 3)
         return (
+            <div>
+                <div className="DApps">
+                    <div >
+                        <h2 className="categories">New dApps</h2>
+                        <Items items = {today}/>
+                    </div>    
+                </div>
             <div className="DApps">
                 {keys.map((category, index) => (
-                <div key={index} >
-                <h2 className="categories">{category}</h2>
-                    <Items key ={index} items = {this.state.myData[category]}/>
-                </div>
+                    <div key={index} >
+                    <h2 className="categories">{category}</h2>
+                        <Items key ={index} items = {this.state.myData[category]}/>
+                    </div>
                 ))}
+            </div>
             </div>
         )
     }
