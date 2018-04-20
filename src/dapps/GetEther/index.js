95bc0ed7d9225943a00ce6a14068f88c8ee3ea0a
Viktor Radchenko <yazexel@gmail.com>
2018-04-20 04:51:24 -0700

    List ether providers and pass wallet address

--- src/dapps/GetEther/index.js
+++ src/dapps/GetEther/index.js
@@ -9,54 +9,44 @@ import {
 } from 'react-router-dom'
 import axios from "axios";
 import ProviderItems from "./components/ProviderItems"
+import getWeb3 from '../../utils/provider'
+
+let address = getWeb3().eth.accounts[0]
 
 const listOfProviders = [
     { 
         name: "Coinbase",
         description: "Cheap way to buy crypto",
         image: "/assets/coinbase.png",
-        countries: new Set([
-            "US", "CA"
-        ]),
-        supportAll: false,
-        url: function(address) {
-            return "https://buy.coinbase.com/widget?code=88d6141a-ff60-536c-841c-8f830adaacfd&crypto_currency=ETH&address=" + address
-        } 
+        // countries: new Set([
+        //     "US", "CA"
+        // ]),
+        supportAll: true,
+        url: "https://buy.coinbase.com/widget?code=88d6141a-ff60-536c-841c-8f830adaacfd&crypto_currency=ETH&address=" + address
     },
     { 
         name: "Changelly",
         description: "Cheap way to buy crypto",
         image: "/assets/changelly.png",
-        countries: new Set([
-            "US", "CA"
-        ]),
-        supportAll: false,
-        url: function(address) {
-            let ref = "968d4f0f0bf9"
-            return "https://changelly.com/widget/v1?auth=email&from=USD&to=ETH&merchant_id=" + ref + "address=" + address + "&ref_id=" + ref + "&color=00cf70"
-        }
+        // countries: new Set([
+        //     "US", "CA"
+        // ]),
+        supportAll: true,
+        url: "https://changelly.com/widget/v1?auth=email&from=USD&to=ETH&merchant_id=968d4f0f0bf9&ref_id=968d4f0f0bf9&color=00cf70&address=" + address
     },
-    { 
-        name: "Indacoin",
-        description: "Cheap way to buy crypto",
-        image: "/assets/indacoin.png",
-        countries: new Set([
-            "US", "CA"
-        ]),
-        supportAll: false,
-        url: function(address) {
-            return "https://indacoin.com/gw/payment_form?partner=trustwallet&cur_to=ETH&amount=100&address=" + address
-        }
-    }
+    // { 
+    //     name: "Indacoin",
+    //     description: "Cheap way to buy crypto",
+    //     image: "/assets/indacoin.png",
+    //     // countries: new Set([
+    //     //     "US", "CA"
+    //     // ]),
+    //     supportAll: true,
+    //     url: function() {
+    //         return "https://indacoin.com/gw/payment_form?partner=trustwallet&cur_to=ETH&amount=100&address=" + address
+    //     }
+    // }
 ]
-
-function generate() {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
 
 class DApps extends React.Component {
 
@@ -83,6 +73,10 @@ class DApps extends React.Component {
             <div>
                 <h2 className="categories">Buy crypto with fiat money</h2>
                 {this.content()}
+                <br />
+                <center>
+                    <p className="media-body center">You almost there, pick a provider to buy ether on your wallet.</p>
+                </center>
             </div>
         )
     }
@@ -97,9 +91,8 @@ class DApps extends React.Component {
             )
         } else {
             let providers = listOfProviders.filter(provider => {
-                return provider.countries.has(country_code)
+                return true //provider.countries.has(country_code)
             });
-            console.log("providers", providers);
             if (providers.length > 0) {
                 return (
                     <div>
