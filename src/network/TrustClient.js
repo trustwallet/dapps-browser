89aa14f4f4698efb57c6fcd0553f4528879cfd3a
Viktor Radchenko <yazexel@gmail.com>
2018-04-24 13:38:24 -0700

    Remove unused methods

--- src/network/TrustClient.js
+++ src/network/TrustClient.js
@@ -3,14 +3,6 @@ import axios from "axios";
 const url = 'https://trust-api.herokuapp.com'
 
 export class TrustClient {
-    fetchList() {
-        return axios.get(url + "/dapps")
-    }
-
-    fetchDApp(id) {
-        return axios.get(url + "/dapps/" + id)
-    }
-    
     fetchDAppsByCategoryID(id, network) {
         return axios.get(url + "/dapps/category/" + id + "?network=" + network)
     }
