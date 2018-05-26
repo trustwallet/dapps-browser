20ba706af6fa90cb581a3c5069e08bea8f89d442
Viktor Radchenko <yazexel@gmail.com>
2018-05-26 13:51:12 -0700

    Use providers as classes

--- src/dapps/GetEther/components/ProviderItem.js
+++ src/dapps/GetEther/components/ProviderItem.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Media } from 'reactstrap';
 
-function ProviderItem() {
-  const item = this.props.item;
-  const url = item.url(this.props.address);
-  return (
-    <a className="providerItem" href={url}>
-      <Media className="mt-1 align media-block">
-        <Media left bottom>
-          <img src={item.image} alt="logo" className="media-logo" />
+class ProviderItem extends React.Component {
+  render() {
+    const item = this.props.item;
+    const url = item.url(this.props.address);
+
+    return (
+      <a className="providerItem" href={url}>
+        <Media className="mt-1 align media-block">
+          <Media left bottom>
+            <img src={item.image} alt="logo" className="media-logo" />
+          </Media>
+          <Media body>
+            <Media heading>
+              {item.name}
+            </Media>
+            {/* <Media>
+                            {item.description}
+                        </Media> */}
+            <Media>
+              <b>Fees</b> : {item.fees}
+            </Media>
+            <Media>
+              <b>Limits </b> : {item.limits}
+            </Media>
+            <Media>
+              <b>Delivery </b> : {item.delivery}
+            </Media>
+          </Media>
         </Media>
-        <Media body>
-          <Media heading>
-            {item.name}
-          </Media>
-          {/* <Media>
-                          {item.description}
-                      </Media> */}
-          <Media>
-            <b>Fees</b> : {item.fees}
-          </Media>
-          <Media>
-            <b>Limits </b> : {item.limits}
-          </Media>
-          <Media>
-            <b>Delivery </b> : {item.delivery}
-          </Media>
-        </Media>
-      </Media>
-    </a>
-  );
+      </a>
+    );
+  }
 }
 
 export default ProviderItem;
--- src/dapps/GetEther/components/ProviderItems.js
+++ src/dapps/GetEther/components/ProviderItems.js
@@ -5,18 +5,20 @@ import {
 } from 'reactstrap';
 import ProviderItem from './ProviderItem';
 
-function ProviderItems() {
-  return (
-    <div>
-      <Row>
-        {this.props.items.map((dapp, index) => (
-          <Col xs="12" sm="6" md="4" key={index}>
-            <ProviderItem item={dapp} key={index} address={this.props.address} />
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
+class ProviderItems extends React.Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          {this.props.items.map((dapp, index) => (
+            <Col xs="12" sm="6" md="4" key={index}>
+              <ProviderItem item={dapp} key={index} address={this.props.address} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default ProviderItems;
