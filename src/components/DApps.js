b8fff7f7f9b85b6ac25fe55b5897251a515eed6c
Mykola <kolya182@gmail.com>
2018-07-12 14:58:14 -0700

    Ignore version 80 and below

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -32,7 +32,7 @@ class DApps extends React.Component {
 
   render() {
     const browserVersion = getTrsutBrowserVersion()
-    if (browserVersion >= 1.68 && isIOS) {
+    if (browserVersion >= 1.80 && isIOS) {
       return (
         <DAppsDisabled />
       )
