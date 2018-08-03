5125a8810319ba6b14eef76ad53fd5098c0b8c78
Mykola <kolya182@gmail.com>
2018-08-03 11:30:06 -0700

    Bump to 1.94

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -32,7 +32,7 @@ class DApps extends React.Component {
 
   render() {
     const browserVersion = getTrsutBrowserVersion()
-    if (browserVersion >= 1.93 && isIOS) {
+    if (browserVersion >= 1.94 && isIOS) {
       return (
         <DAppsDisabled />
       )
