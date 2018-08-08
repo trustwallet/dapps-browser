a87ec2f43a35d73a7545b946ea37bbce02e7371d
Mykola <kolya182@gmail.com>
2018-08-08 08:07:15 -0700

    Downgrade 1.5

--- src/components/DApps.js
+++ src/components/DApps.js
@@ -32,7 +32,7 @@ class DApps extends React.Component {
 
   render() {
     const browserVersion = getTrsutBrowserVersion()
-    if (browserVersion >= 1.103 && isIOS) {
+    if (browserVersion >= 1.5 && isIOS) {
       return (
         <DAppsDisabled />
       )
