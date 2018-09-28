72bc134c02b99274f32800b9eea1dba65e1e11bd
Mykola <3277207+kolya182@users.noreply.github.com>
2018-09-27 18:07:37 -0700

    Do not extend native objects

--- src/components/DAppCard.js
+++ src/components/DAppCard.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { Media } from 'reactstrap';
-
-String.prototype.trunc = function trunc(n, useWordBoundary) {
-  if (this.length <= n) { return this; }
-  const subString = this.substr(0, n - 1);
-  return `${useWordBoundary
-    ? subString.substr(0, subString.lastIndexOf(' '))
-    : subString} ... `;
-};
+import { trunc } from '../utils/utils';
 
 class DAppCard extends React.Component {
   render() {
     const item = this.props.item;
     const url = item.url;
+    console.log({item})
     return (
       <Media tag="a" href={url}>
         <div className="card-body">
           <img src={item.image} alt="logo" className="media-card-logo" />
           <p className="card-body-title">{item.name}</p>
-          <p className="card-body-description">{item.description.trunc(80, true)}</p>
+          <p className="card-body-description">{trunc(item.description, 80, true)}</p>
         </div>
       </Media>
     );
