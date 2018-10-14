7ef23ab465ac65c535f37de100355e2d9918911e
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-14 10:03:59 -0700

    Temp disable webworker

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
