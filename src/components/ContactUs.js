95ff6e0c982d9be610380fefc78e98a11b40cf0e
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-13 18:48:01 -0700

    Fix warning
    https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,15 +7,15 @@ function ContactUs() {
       <h1>Contact Us</h1>
       <ListGroupItem>
         <i className="fab fa-app-store-ios fa-2x" />
-        <a className="contact-us-div" href="https://goo.gl/forms/GEVXRc2QSLVuOnMb2" target="_blank">Submit DApp</a>
+        <a className="contact-us-div" href="https://goo.gl/forms/GEVXRc2QSLVuOnMb2" rel="noopener noreferrer" target="_blank">Submit DApp</a>
       </ListGroupItem>
       <ListGroupItem>
         <i className="fab fa-twitter fa-2x" />
-        <a className="contact-us-div" href="https://twitter.com/TrustWalletApp" target="_blank">Twitter</a>
+        <a className="contact-us-div" href="https://twitter.com/TrustWalletApp" rel="noopener noreferrer" target="_blank">Twitter</a>
       </ListGroupItem>
       <ListGroupItem>
         <i className="fab fa-facebook fa-2x" />
-        <a className="contact-us-div" href="https://www.facebook.com/trustwalletapp/" target="_blank">Facebook</a>
+        <a className="contact-us-div" href="https://www.facebook.com/trustwalletapp/" rel="noopener noreferrer" target="_blank">Facebook</a>
       </ListGroupItem>
       <ListGroupItem>
         <i className="fas fa-envelope fa-2x" />
