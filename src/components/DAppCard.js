fdb225b38557b0a64d908ebeb1dea9ecb61936c3
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-13 18:34:56 -0700

    Fix warning

--- src/components/DAppCard.js
+++ src/components/DAppCard.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { Media } from 'reactstrap';
-
-String.prototype.trunc = function trunc(n, useWordBoundary) {
-  if (this.length <= n) { return this; }
-  const subString = this.substr(0, n - 1);
-  return `${useWordBoundary
-    ? subString.substr(0, subString.lastIndexOf(' '))
-    : subString} ... `;
-};
+import { trunc } from '../utils/utils';
 
 class DAppCard extends React.Component {
   render() {
@@ -18,7 +11,7 @@ class DAppCard extends React.Component {
         <div className="card-body">
           <img src={item.image} alt="logo" className="media-card-logo" />
           <p className="card-body-title">{item.name}</p>
-          <p className="card-body-description">{item.description.trunc(80, true)}</p>
+          <p className="card-body-description">{trunc(item.description, 80, true)}</p>
         </div>
       </Media>
     );
