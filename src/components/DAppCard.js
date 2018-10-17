2c4f45537e646c98c8a63b37cbeac6872b9d41e8
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-17 13:21:57 -0700

    Destructuring

--- src/components/DAppCard.js
+++ src/components/DAppCard.js
@@ -4,14 +4,14 @@ import { trunc } from '../utils/utils';
 
 class DAppCard extends React.Component {
   render() {
-    const item = this.props.item;
-    const url = item.url;
+    const { url, image, name, description} = this.props.item
+
     return (
       <Media tag="a" href={url}>
         <div className="card-body">
-          <img src={item.image} alt="logo" className="media-card-logo" />
-          <p className="card-body-title">{item.name}</p>
-          <p className="card-body-description">{trunc(item.description, 80, true)}</p>
+          <img src={image} alt="logo" className="media-card-logo" />
+          <p className="card-body-title">{name}</p>
+          <p className="card-body-description">{trunc(description, 80, true)}</p>
         </div>
       </Media>
     );
