ce30a3972f2425645a04ab75eb5bcbea1c688b88
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-17 14:02:42 -0700

    New dapps => dapp page

--- src/components/DAppCard.js
+++ src/components/DAppCard.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Media } from 'reactstrap';
+import { Media} from 'reactstrap';
 import { trunc } from '../utils/utils';
+import { Link } from "react-router-dom";
 
 class DAppCard extends React.Component {
   render() {
-    const { url, image, name, description} = this.props.item
+    const { image, name, description} = this.props.item
 
     return (
-      <Media tag="a" href={url}>
-        <div className="card-body">
-          <img src={image} alt="logo" className="media-card-logo" />
-          <p className="card-body-title">{name}</p>
-          <p className="card-body-description">{trunc(description, 80, true)}</p>
-        </div>
-      </Media>
+      <div>
+        <Link to={{
+          pathname: `/${name}`,
+          state: {
+            dapp: this.props.item
+          }
+        }}>
+          <Media>
+            <div className="card-body">
+              <img src={image} alt="logo" className="media-card-logo" />
+              <p className="card-body-title">{name}</p>
+              <p className="card-body-description">{trunc(description, 80, true)}</p>
+            </div>
+          </Media>
+        </Link>
+      </div>
     );
   }
 }
