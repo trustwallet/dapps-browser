66b2298e6d485fff2879456fd23965e647ded3f2
Mykola <3277207+kolya182@users.noreply.github.com>
2018-10-23 22:45:26 -0700

    Show url instead of name

--- src/components/DAppPage.js
+++ src/components/DAppPage.js
@@ -3,19 +3,18 @@ import { CardText, CardImg, Button} from "reactstrap";
 
 class DAppPage extends React.Component {
     render() {
-        console.log(this.props)
+        console.log(`DAppPage - `,this.props)
         const { 
             description,
             image,
             page_image, 
-            name, 
             url } = this.props.location.state.dapp
 
         return (
             <div>
               <CardImg width="100%" src={page_image ? page_image : image} alt="DApp page"></CardImg>
               <CardText width="100%" color="danger">{description}</CardText>
-              <Button outline size="sm" color="primary" href={url}>visit {name}</Button>
+              <Button outline size="sm" color="primary" href={url}>Visit {url}</Button>
             </div>
         )
     }
