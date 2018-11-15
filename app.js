0a99dd108114a55377ddd1f465100a0fb619e7da
Mykola <3277207+kolya182@users.noreply.github.com>
2018-11-14 18:19:01 -0800

    Serve index.html

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'), (err) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 server.listen(port, () => console.log(`Running on localhost:${port}`));
