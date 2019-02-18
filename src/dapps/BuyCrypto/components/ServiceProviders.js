34306009d63cd6ebae558b4a08137fe73da91830
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-02-18 12:24:29 -0800

    Update ServiceProviders.js

--- src/dapps/BuyCrypto/components/ServiceProviders.js
+++ src/dapps/BuyCrypto/components/ServiceProviders.js
@@ -9,16 +9,16 @@ export default [
     fees: 'Fees 5%',
     delivery: '10-30 minute settlement',
     currencies: new Set(["BTC", "BCH", "ETH", "LTC"])
-  },
-  {
-    name: 'Wyre',
-    description: 'Buy crypto with US bank accounts',
-    image: '/assets/wyre.png',
-    url: function url(address) {
-      return `https://buy.coinbase.com/widget?code=88d6141a-ff60-536c-841c-8f830adaacfd&crypto_currency=ETH&address=${address}`;
-    },
-    fees: 'Fees 1%',
-    delivery: '1-3 days settlement',
-    currencies: new Set(["BTC", "ETH", "DAI"])
-  },
+  }
+  // {
+  //   name: 'Wyre',
+  //   description: 'Buy crypto with US bank accounts',
+  //   image: '/assets/wyre.png',
+  //   url: function url(address) {
+  //     return `https://buy.coinbase.com/widget?code=88d6141a-ff60-536c-841c-8f830adaacfd&crypto_currency=ETH&address=${address}`;
+  //   },
+  //   fees: 'Fees 1%',
+  //   delivery: '1-3 days settlement',
+  //   currencies: new Set(["BTC", "ETH", "DAI"])
+  // },
 ];
